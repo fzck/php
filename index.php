016e2945e0ac4d61fa4e7b4cf91151afbeae0371
fzck <flzauleck@up.edu.ph>
2018-03-15 20:39:58 +0800

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,21 @@
 <body style"background-color:#7f8c8d">
   
     <div id="main-wrapper">
-  </div>
+      <center>
+        <h2>Login Form<h2>
+        <img src="img/avatar.png" class="avatar"/>
+      </center>  
+ 
+        
+  <form class="myform" action="index.php" method="POST">
+    <label><b>Username:</b></label> <br>
+    <input type="text" class="inputvalues" placeholder="Type your username"/>
+    
+    <label><b>Password:</b></label> <br>
+    <input type="password" class="inputvalues" placeholder="Type your password"/>
+    <input type="submit" id="login_btn" value="Login"/> 
+    <input type="button" id="register_btn" value="Login"/> 
+   </form>
+    </div>
 </body>
 </html>
