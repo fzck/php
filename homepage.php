35eba07db31e6bcce9a22a0dde4ada2302eb431d
fzck <flzauleck@up.edu.ph>
2018-03-15 20:51:09 +0800

    Create homepage.php

---
+++ homepage.php
@@ -0,0 +1,28 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Home Page</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body style"background-color:#7f8c8d">
+  
+    <div id="main-wrapper">
+      <center>
+        <h2>Home Page</h2>
+        <h3>Welcome User</h3>
+        <img src="img/avatar.png" class="avatar"/>
+      </center>  
+ 
+        
+  <form class="myform" action="homepage.php" method="POST">
+   
+    <input type="submit" id="logout_btn" value="Log Out"/> 
+
+   </form>
+    </div>
+</body>
+</html>
